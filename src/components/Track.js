9f896067de77712566386d6e64759e62f973097c
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-06-27 17:05:47 +0200

    Tracks container

---
+++ src/components/Track.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Track = () => {
+  return <h4>Hello</h4>;
+};
+export default Track;
--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,11 +1,23 @@
 import React, { useContext } from "react";
 import { Context } from "./../context";
 import Spinner from "./Spinner";
-
+import Track from "./Track";
 const Tracks = () => {
   const [state] = useContext(Context);
   const { tracks, heading } = state;
 
-  return <Spinner />;
+  if (tracks === undefined || tracks.length === 0) return <Spinner />;
+  else {
+    return (
+      <>
+        <h3 className="text-center mb-4">{heading}</h3>
+        <div className="row">
+          {tracks.map(item => (
+            <Track key={item.track.track_id} track={item.track} />
+          ))}
+        </div>
+      </>
+    );
+  }
 };
 export default Tracks;
