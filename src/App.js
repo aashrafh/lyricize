9885ea3d57f92a0747d38131236fac2395f8da58
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-06-27 02:49:59 +0200

    navbar and setup react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/layout/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-
 function App() {
-  return <h1>hell</h1>;
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <div className="app">
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-primary mb-5">
+      <a className="navbar-brand mb-0 h1 mx-auto" href="/">
+        Lyricize
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
